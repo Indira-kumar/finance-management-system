3e9d2c4c31f93c13f15e2452f1714e4d4e6c2303
Indira Kumar <indirakumarak@gmail.com>
2023-05-12 12:56:49 +0530

    update - index route with all routers

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,15 @@
 import { Router } from "express";
+import dailyCollectionRouter from "./dailyCollection.js";
+import lineMemberRouter from "./lineMember.js";
+import lineRouter from "./line.js";
+import loanRouter from "./loan.js";
+import totalCollectionRouter from "./totalCollection.js";
 const router = Router();
 
+router.use("/dailycollection", dailyCollectionRouter)
+router.use("/line", lineRouter )
+router.use("/linemember", lineMemberRouter)
+router.use("/loan", loanRouter )
+router.use("/totalcollection", totalCollectionRouter )
 
 export default router;
