ead5a61bb0d5c2142c6d60b086121bb14d9c9c25
Indira Kumar <indirakumarak@gmail.com>
2023-05-12 13:16:50 +0530

    update - total collection route controllers

--- routes/totalCollection.js
+++ routes/totalCollection.js
@@ -1,13 +1,13 @@
 import { Router } from "express";
-
+import {createTotalCollection, getTotalCollections, getTotalCollectionByDate, updateTotalCollection, deleteTotalCollection} from "../controllers/totalCollectionController.js";
 const totalCollectionRouter = Router();
 
-totalCollectionRouter.post("/");
-totalCollectionRouter.get("/");
-totalCollectionRouter.get("/:date");
-totalCollectionRouter.put("/", );
-totalCollectionRouter.delete("/:date");
-totalCollectionRouter.patch("/:date");
+totalCollectionRouter.post("/", createTotalCollection);
+totalCollectionRouter.get("/", getTotalCollections);
+totalCollectionRouter.get("/:date", getTotalCollectionByDate);
+totalCollectionRouter.put("/", updateTotalCollection);
+totalCollectionRouter.delete("/:date", deleteTotalCollection);
+// totalCollectionRouter.patch("/:date", updateTotalCollection);
 
 
 export default totalCollectionRouter;
