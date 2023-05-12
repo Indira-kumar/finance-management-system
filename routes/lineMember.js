fc6942200a82b2e9f5eba4d3cac7b06837cac7b1
Indira Kumar <indirakumarak@gmail.com>
2023-05-12 13:02:09 +0530

    update - lineMember route controllers

--- routes/lineMember.js
+++ routes/lineMember.js
@@ -1,13 +1,13 @@
 import { Router } from "express";
-
+import {createLineMember, getAllLineMember, getLineMemberByEmail, updateLineMember, updateLineMemberByEmail,deleteLineMemberByEmail} from "../controllers/lineMemberController.js"
 const lineMemberRouter = Router();
 
-lineMemberRouter.post("/");
-lineMemberRouter.get("/");
-lineMemberRouter.get("/:email");
-lineMemberRouter.put("/", );
-lineMemberRouter.delete("/:email");
-lineMemberRouter.patch("/:email");
+lineMemberRouter.post("/", createLineMember);
+lineMemberRouter.get("/", getAllLineMember);
+lineMemberRouter.get("/:email", getLineMemberByEmail);
+lineMemberRouter.put("/", updateLineMember);
+lineMemberRouter.delete("/:email", deleteLineMemberByEmail);
+lineMemberRouter.patch("/:email", updateLineMemberByEmail);
 
 
 export default lineMemberRouter;
