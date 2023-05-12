5171b5008f76231e901ac887d10acad521f4484c
Indira Kumar <indirakumarak@gmail.com>
2023-05-12 13:14:12 +0530

    update - loan route controllers

--- routes/loan.js
+++ routes/loan.js
@@ -1,13 +1,13 @@
 import { Router } from "express";
-
+import {createLoan, getLoan, getLoans, updateLoan, deleteLoan } from "../controllers/loanController.js";
 const loanRouter = Router();
 
-loanRouter.post("/");
-loanRouter.get("/");
-loanRouter.get("/:loanNumber");
-loanRouter.put("/", );
-loanRouter.delete("/:loanNumber");
-loanRouter.patch("/:loanNumber");
+loanRouter.post("/", createLoan);
+loanRouter.get("/", getLoans);
+loanRouter.get("/:loanNumber", getLoan);
+loanRouter.put("/", updateLoan);
+loanRouter.delete("/:loanNumber", deleteLoan);
+loanRouter.patch("/:loanNumber", updateLoan);
 
 
 export default loanRouter;
