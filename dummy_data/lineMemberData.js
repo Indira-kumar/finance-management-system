017083958acfcbd44fd8f867ce3f4a4372e230aa
Indira Kumar <indirakumarak@gmail.com>
2023-05-15 09:16:36 +0530

    <create> : dummy data

---
+++ dummy_data/lineMemberData.js
@@ -0,0 +1,9 @@
+export default lineMemberData = [
+    {
+        id: "1",
+        member_name: "Dharani",
+        address: "dharanikanagaraj97@gmail.com",
+        phone_no: "9500593041",
+        password: "Lineboy@1234"
+      }
+];
