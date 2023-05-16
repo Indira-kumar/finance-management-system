43c1d732191c464f30cc2621029143a39e516441
Indira Kumar <indirakumarak@gmail.com>
2023-05-16 16:59:57 +0530

    <update> : with automatic loan_no and user_no

--- models/Loan.js
+++ models/Loan.js
@@ -1,14 +1,15 @@
 import mongoose from "mongoose";
 
-const LoanSchema = new mongoose.Schema({
+const loanSchema = new mongoose.Schema({
   loan_no: { type: String, required: true },
   user_no: { type: String, required: true },
   name: { type: String, required: true },
   address: { type: String, required: true },
   phone_no: { type: String, required: true },
   order_no: { type: String, required: true },
-  created_on: { type: Date, required: true, default: Date.now },
-  updated_on: { type: Date, required: true, default: Date.now },
+  date:{ type: Date, required: true},
+  created_on: { type: Date, required: true, default: new Date() },
+  updated_on: { type: Date, required: true, default: new Date() },
   loan_amount: { type: Number, required: true },
   pay_amount:{type:Number, required:true}, //send in UI by dividing the loan_amount*interest/100
   balance:{type:Number, required: true}, //send in UI, value same as loan_amount
@@ -23,6 +24,54 @@ const LoanSchema = new mongoose.Schema({
   line_name: { type: String, required: true}
 });
 
-const Loan = mongoose.model('Loan', LoanSchema);
+//logic to make sure that loan_no is consecutive and unique wrt a line_name  
+loanSchema.pre('save', async function(next) {
+  try {
+    // Check if loan_no is already assigned
+    if (this.loan_no) {
+      return next();
+    }
+    
+    // Find the highest loan_no for the current line_name
+    const highestLoan = await this.constructor.findOne({ line_name: this.line_name })
+      .sort({ loan_no: -1 })
+      .exec();
+    
+    // Generate a new loan_no
+    let newLoanNo = '';
+    if (highestLoan) {
+      // If there are existing loans for this line_name, increment the highest loan_no
+      const currentLoanNo = parseInt(highestLoan.loan_no, 10);
+      newLoanNo = `${(currentLoanNo + 1).toString()}`;
+    } else {
+      // If there are no existing loans for this line_name, start with 0001
+      newLoanNo = '1';
+    }
+    
+    // Assign the new loan_no to the loan document
+    this.loan_no = newLoanNo;
+    next();
+  } catch (err) {
+    next(err);
+  }
+});
+
+// pre middleware to set user_no before saving a new loan
+loanSchema.pre('save', async function (next) {
+  const loan = this;
+  if (!loan.user_no) {
+    try {
+      const latestLoan = await Loan.findOne({ name: loan.name, line_name:loan.line_name })
+        .sort('-user_no')
+        .select('user_no');
+      loan.user_no = latestLoan ? latestLoan.user_no + 1 : 1;
+      next();
+    } catch (err) {
+      next(err);
+    }
+  }
+});
+
+const Loan = mongoose.model('Loan', loanSchema);
 
 export default Loan;
