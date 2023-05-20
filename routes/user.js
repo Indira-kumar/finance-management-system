cfefc64ad68d435ece377a39b4ea06994aadda49
Indira Kumar <indirakumarak@gmail.com>
2023-05-21 01:42:46 +0530

    <update> : user route, validation for phone num

--- routes/user.js
+++ routes/user.js
@@ -2,8 +2,9 @@ import { Router } from "express";
 import {register, login, logout, updateUserByPhoneNum} from "../controllers/userController.js"
 const userRouter = Router();
 
-userRouter.post("/", register);
+userRouter.post("/register", register);
 userRouter.get("/logout", logout);
+userRouter.post("/login", login)
 userRouter.put("/", updateUserByPhoneNum);
 userRouter.delete("/", logout); // have to check which HTTP request for logging out
 
--- utils/validation.js
+++ utils/validation.js
@@ -11,7 +11,7 @@ export const registerValidation = (data) => {
   }
 
   // const passwordRegex = /(?=.*[a-z])(?=.*[A-Z])(?=.*\W).{8,20}/;
-  const passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,10}$/
+  const passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,20}$/
   if (!passwordRegex.test(password)) {
     errors.push({
       msg: "password should have one small case, a capital letter, a symbol and a number and also the length should be greater than 8 and less than 20",
@@ -20,7 +20,7 @@ export const registerValidation = (data) => {
 
   }
 
-  const phoneRegex = /^(\+?91|0)?[6789]\d{9}$/;
+  const phoneRegex = /^[6-9]\d{9}$/;
   if (!phoneRegex.test(phone_no)) {
     errors.push({ msg: "Enter a valid phone number" });
     return errors
@@ -32,7 +32,8 @@ export const registerValidation = (data) => {
 export const loginValidation = (data) => {
   const { phone_no} = data;
   const errors =[]
-  const phoneRegex = /^(\+?91|0)?[6789]\d{9}$/;
+  const phoneRegex = /^[6-9]\d{9}$/;
+
   if (!phoneRegex.test(phone_no)) {
     errors.push({ msg: "Enter a valid phone number" });
     return errors
